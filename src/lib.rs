189458cd7a8f02d23305bd424dad1fa9b13034dc
Grics Balázs <gricsb@gmail.com>
2022-07-08 22:56:49 +0200

    working :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::iter::Iterator;
+use std::{iter::Iterator, collections::VecDeque, fmt::Display,};
+use rand::prelude::*;
+use rand_xorshift::XorShiftRng;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Direction {
     Up,
     Down,
@@ -20,35 +22,55 @@ impl Direction {
     }
 }
 
-pub trait Segment: Copy {
-    fn dir(self) -> Direction;
-    fn len(self) -> usize;    
-}
-impl<T: Copy> Segment for T where T: Into<(Direction, usize)> {
-    fn dir(self) -> Direction {
-        self.into().0
-    }
-
-    fn len(self) -> usize {
-        self.into().1
-    }
+pub trait Segment {
+    fn dir(&self) -> Direction;
+    fn len(&self) -> usize;
 }
 
+pub type Seg = (Direction, usize);
+
+impl Segment for (Direction, usize) {
+    #[inline]
+    fn dir(&self) -> Direction {
+        self.0
+    }
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.1
+    }
+}
 
 pub trait SegmentIter<'a, S: Segment + 'a>: Iterator<Item = &'a S> {}
 impl<'a, T, S: Segment + 'a> SegmentIter<'a, S> for T where T: Iterator<Item = &'a S> {}
 
-pub type Pos = (isize, isize);
-trait NextPos {
-    fn next_pos(self, dir: Direction) -> Pos;
+pub trait Position {
+    fn x(&self) -> isize;
+    fn y(&self) -> isize;
 }
-impl<T> NextPos for T
-where
-    T: Into<Pos>,
-{
-    fn next_pos(self, dir: Direction) -> Pos {
+
+// (x, y)
+pub type Pos = (isize, isize);
+
+impl Position for (isize, isize) {
+    #[inline]
+    fn x(&self) -> isize {
+        self.0
+    }
+
+    #[inline]
+    fn y(&self) -> isize {
+        self.1
+    }
+}
+
+trait NextPos<P: Position> {
+    fn next_pos(self, dir: Direction) -> P;
+}
+impl NextPos<(isize, isize)> for (isize, isize) {
+    fn next_pos(self, dir: Direction) -> (isize, isize) {
         use Direction::*;
-        let mut pos: Pos = self.into();
+        let mut pos = self.clone();
         match dir {
             Up => pos.1 += 1,
             Down => pos.1 -= 1,
@@ -59,33 +81,30 @@ where
     }
 }
 
-pub struct PosIter<I, S> {
+pub struct PosIter<'a, I, S, P> {
     seg_itr: I,
-    seg: Option<S>,
-    pos: Pos,
+    seg: Option<&'a S>,
+    pos: P,
     idx: usize,
-    is_last: bool,
 }
 
-impl<'a, S: Segment + 'a, I: SegmentIter<'a, S>> Iterator for PosIter<I, S> {
-    type Item = Pos;
+impl<'a, S, I, P> Iterator for PosIter<'a, I, S, P>
+where
+    S: Segment + 'a,
+    I: SegmentIter<'a, S>,
+    P: Position + NextPos<P> + Copy,
+{
+    type Item = P;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             if self.seg.is_none() {
                 if let Some(nxt_seg) = self.seg_itr.next() {
-                    self.seg = Some(*nxt_seg);
+                    self.seg = Some(nxt_seg);
                     continue;
-                } else {
-                    if !self.is_last {
-                        self.is_last = true;
-                        break Some(self.pos);
-                    } else {
-                        break None;
-                    }
-                }
+                } else { break None; }
             } else {
-                if self.idx + 1 >= self.seg.unwrap().len() {
+                if self.idx >= self.seg.unwrap().len() {
                     self.idx = 0;
                     self.seg = None;
                     continue;
@@ -100,31 +119,50 @@ impl<'a, S: Segment + 'a, I: SegmentIter<'a, S>> Iterator for PosIter<I, S> {
     }
 }
 
-impl<'a, S: Segment + 'a, I: SegmentIter<'a, S>> PosIter<I, S> {
-    pub fn new(seg_iter: I, start: Pos) -> PosIter<I, S> {
+impl<'a, S, I, P> PosIter<'a, I, S, P>
+where
+    S: Segment + 'a,
+    I: SegmentIter<'a, S>,
+    P: Position,
+{
+    pub fn new(seg_iter: I, start: P) -> PosIter<'a, I, S, P> {
         PosIter {
             seg_itr: seg_iter,
             seg: None,
             pos: start,
             idx: 0,
-            is_last: false,
         }
     }
 }
+pub trait PosIterBuilder<'a, S, I, P>
+where
+    S: Segment + 'a,
+    I: SegmentIter<'a, S>,
+    P: Position,
+{
+    fn iter_from_start(self, pos: P) -> PosIter<'a, I, S, P>;
+}
+
+impl<'a, T, S, I> PosIterBuilder<'a, S, I, Pos> for T 
+where 
+    S: Segment + 'a,
+    I: SegmentIter<'a, S>,
+    T: IntoIterator<Item = &'a S, IntoIter = I>,
+{
+    fn iter_from_start(self, pos: Pos) -> PosIter<'a, I, S, Pos> {
+        PosIter::new(self.into_iter(), pos)
+    }
+}
+
 
 #[cfg(test)]
-mod test {
-    use crate::{Direction, Pos, PosIter};
+mod pos_iter_test {
+    use crate::{Direction, Pos, PosIterBuilder, Seg};
 
     #[test]
-    fn pos_iter_test() {
+    fn test_pos_iter() {
         use Direction::*;
-        let segs: &[(Direction, usize)] = &[
-            (Right, 3),
-            (Down, 2),
-            (Right, 3),
-            (Down, 4),
-        ];
+        let segs: &[Seg] = &[(Right, 2), (Down, 1), (Right, 2), (Down, 3)];
 
         let expected: &[Pos] = &[
             (4, -4),
@@ -135,12 +173,246 @@ mod test {
             (0, -3),
             (0, -2),
             (0, -1),
-            (0, 0),
         ];
 
-        let itr = PosIter::new(segs.iter(), (4, -4));
-        let actual: Vec<Pos> = itr.collect();
+        let actual: Vec<Pos> = segs.iter_from_start((4, -4)).collect();
 
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_one_len_segment() {
+        use Direction::*;
+        let segs: &[Seg] = &[(Left, 1), (Up, 9)];
+
+        let expected: &[Pos] = &[
+            (-1,1),
+            (0,1),
+            (0,0),
+            (0,-1),
+            (0,-2),
+            (0,-3),
+            (0,-4),
+            (0,-5),
+            (0,-6),
+            (0,-7)
+        ];
+
+        let actual: Vec<Pos> = segs.iter_from_start((-1,1)).collect();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+// (col, row)
+pub type Cell = (u16, u16);
+
+pub trait Cellular: From<Cell> {
+    fn col(&self) -> u16;
+    fn row(&self) -> u16;
+}
+
+impl Cellular for Cell {
+    #[inline]
+    fn col(&self) -> u16 {
+        self.0
+    }
+
+    #[inline]
+    fn row(&self) -> u16 {
+        self.1
+    }
+}
+
+pub struct CellIter<I, C> {
+    pos_itr: I,
+    bound: C,
+}
+
+impl<I, P, C> Iterator for CellIter<I, C>
+where
+    P: Position,
+    I: Iterator<Item = P>,
+    C: Cellular,
+{
+    type Item = C;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pos_itr.next().map(|p| {
+            (
+                (self.bound.col() as isize / 2 + (p.x() * 2)).rem_euclid(self.bound.col() as isize)
+                    as u16,
+                (self.bound.row() as isize / 2 - p.y()).rem_euclid(self.bound.row() as isize)
+                    as u16,
+            )
+                .into()
+        })
+    }
+}
+
+impl<I, P, C> CellIter<I, C>
+where
+    P: Position,
+    I: Iterator<Item = P>,
+    C: Cellular,
+{
+    pub fn new(pos_itr: I, bound: C) -> Self {
+        Self { pos_itr, bound }
+    }
+}
+
+impl<'a, S, I> PosIter<'a, I, S, Pos>
+where
+    S: Segment + 'a,
+    I: SegmentIter<'a, S>,
+{
+    pub fn within_bound(self, bnd: Cell) -> CellIter<PosIter<'a, I, S, Pos>, Cell> {
+        CellIter::new(self, bnd)
+    }
+} 
+
+#[cfg(test)]
+mod cell_iter_test {
+    use crate::{Cell, PosIterBuilder, Direction, Seg};
+
+    #[test]
+    fn test_cell_iter() {
+        use Direction::*;
+        let segs: &[Seg] = &[(Up, 2), (Left, 3), (Up, 2), (Right, 6)];
+
+        let expected: &[Cell] = &[
+            (10, 5),
+            (10, 6),
+            (10, 7),
+            (12, 7),
+            (14, 7),
+            (16, 7),
+            (16, 8),
+            (16, 9),
+            (14, 9),
+            (12, 9),
+            (10, 9),
+            (8, 9),
+            (6, 9),
+        ];
+
+        let actual: Vec<Cell> = segs
+            .iter_from_start((0, 0))
+            .within_bound((20, 10))
+            .collect();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+pub struct Snake {
+    head: Pos,
+    segs: VecDeque<Seg>
+}
+
+impl Snake {
+
+    pub fn new<P: Into<Pos>>(start: P, len: usize) -> Snake {
+        Self { head: start.into(), segs: VecDeque::from([(Direction::Up, len)]) }
+    }
+
+    pub fn snaking(&mut self, new_dir: Option<Direction>) {
+        if let (Some(new_dir), Some(top_seg)) = (new_dir, self.segs.front()) {
+            if top_seg.dir() != new_dir && top_seg.dir() != new_dir.opposite() {
+                self.segs.push_front((new_dir, 0));
+            }
+        }
+
+        if let Some(top_seg) = self.segs.front_mut() {
+            self.head = self.head.next_pos(top_seg.dir());
+            top_seg.1 += 1;
+        }
+        if let Some(mut lst_seg) = self.segs.pop_back() {
+            if lst_seg.len() > 1 {
+                lst_seg.1 -= 1;
+                self.segs.push_back(lst_seg);
+            } else {
+                drop(lst_seg);
+            }
+        }
+    }
+
+    pub fn is_collide(&self) -> bool {
+        self.segs.iter_from_start(self.head)
+        .skip(1)
+        .any(|p| p == self.head)
+    }
+
+    pub fn grow(&mut self) {
+        if let Some(lst_seg) = self.segs.back_mut() {
+            lst_seg.1 += 1;
+        }
+    }
+
+    pub fn formatter(&self, bound: Cell, elems: [char; 5]) -> SnakeFormatter<'_> {
+        SnakeFormatter { snk: self, bnd: bound, lms: elems }
+    }
+}
+
+pub struct SnakeFormatter<'a> { 
+    snk: &'a Snake,
+    bnd: Cell,
+    lms: [char; 5]
+}
+
+impl<'a> Display for SnakeFormatter<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let head_char = if let Some((head_dir, _)) = self.snk.segs.front() {
+            self.lms[*head_dir as usize]
+        } else {
+            self.lms[4]
+        };
+
+        let mut cell_itr = self.snk.segs
+            .iter_from_start(self.snk.head)
+            .within_bound(self.bnd);
+
+        if let Some(head_cell) = cell_itr.next() {
+            write!(f,"\x1b[{};{}H{}", head_cell.row(), head_cell.col(), head_char)?;
+            for body_cell in cell_itr {
+                write!(f,"\x1b[{};{}H{}", body_cell.row(), body_cell.col(), self.lms[4])?;
+            }
+            Ok(())
+        } else {
+            write!(f,"")
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Food { 
+    pos: Cell,
+    bnd: Cell
+}
+
+impl Food {
+    pub fn somewhere_within(bound: Cell) -> Self {
+        let mut rng = XorShiftRng::from_entropy();
+        Self { 
+            bnd: bound,
+            pos: (
+                ((rng.next_u32() / 3 * 2 + 1)).rem_euclid(bound.col() as u32) as u16,
+                (rng.next_u32()).rem_euclid(bound.row() as u32) as u16,
+            ),
+        }
+    }
+
+    pub fn is_eaten_by(&self, snake: &Snake) -> bool {
+        snake.segs
+        .iter_from_start(snake.head)
+        .within_bound(self.bnd)
+        .next()
+        .map_or(false, |head_cell| head_cell == self.pos)
+    }    
+}
+
+impl Display for Food {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"\x1b[{};{}H@", self.pos.row(), self.pos.col())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,157 @@
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode},
+    queue, style,
+    terminal::{self, disable_raw_mode, enable_raw_mode, ClearType},
+};
+use std::{
+    io::{self, stdout, Write},
+    process,
+    time::Duration,
+};
+
+use rs_snake::{Snake, Food};
+
+pub const DEFAULT_PARTS: [char; 5] = ['⮝', '⮟', '⮜', '➤', '*'];
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = with_blank_screen(splash) {
+        eprintln!("unable to print screen: {}", err);
+        process::exit(1);
+    }
+}
+
+type Print = fn() -> io::Result<Screen>;
+enum Screen {
+    Next(Print),
+    Exit,
+}
+
+fn splash() -> io::Result<Screen> {
+    let (col, row) = terminal::size()?;
+    queue!(
+        stdout(),
+        cursor::MoveTo(col / 2 - 9, row / 2 - 1),
+        style::Print("Snakes are Snakeing!"),
+        cursor::MoveDown(1),
+        cursor::MoveLeft(9),
+        cursor::SavePosition,
+        style::Print("Press enter to start...."),
+        cursor::RestorePosition,
+        cursor::MoveDown(1),
+        style::Print("q or ESC to exit.")
+    )?;
+    stdout().flush()?;
+    loop {
+        match event::read()? {
+            Event::Key(key) if key.code == KeyCode::Esc || key.code == KeyCode::Char('q') => {
+                break Ok(Screen::Exit)
+            }
+            Event::Key(key) if key.code == KeyCode::Enter => break Ok(Screen::Next(game_loop)),
+            _ => continue,
+        }
+    }
+}
+
+fn game_loop() -> io::Result<Screen> {
+    let term_size = terminal::size()?;
+    let mut snake = Snake::new((0,0), 10);
+
+    let mut food = Food::somewhere_within(term_size);
+
+    loop {
+        
+        let snk_fmtr = snake.formatter(term_size, DEFAULT_PARTS);
+        queue!(
+            stdout(),
+            terminal::Clear(ClearType::All),
+            style::Print(format!("{}", snk_fmtr)),
+            style::Print(format!("{}", food)),
+        )?;
+        stdout().flush()?;        
+
+        snake.snaking(if event::poll(Duration::from_millis(250))? {
+            use rs_snake::Direction::*;
+
+            match event::read()? {
+                Event::Key(key) if key.code == KeyCode::Esc || key.code == KeyCode::Char('q') => {
+                    break Ok(Screen::Exit);
+                },
+                Event::Key(key) if key.code == KeyCode::Up => Some(Up),
+                Event::Key(key) if key.code == KeyCode::Down => Some(Down),
+                Event::Key(key) if key.code == KeyCode::Left => Some(Left),
+                Event::Key(key) if key.code == KeyCode::Right => Some(Right),
+                _ => None,
+            }
+        } else {
+            None
+        });
+
+        if snake.is_collide() {
+            break Ok(Screen::Next(you_die));
+        }
+        if food.is_eaten_by(&snake) {
+            snake.grow();
+            food = Food::somewhere_within(term_size)
+        }
+
+    }
+}
+
+fn you_die() -> io::Result<Screen> {
+    let (col, row) = terminal::size()?;
+    queue!(
+        stdout(),
+        terminal::Clear(ClearType::All),
+        cursor::MoveTo(col / 2 - 3, row / 2),
+        style::Print("You DIE!")
+    )?;
+    stdout().flush()?;
+    loop {
+        match event::read()? {
+            Event::Key(key) if key.code == KeyCode::Enter => break Ok(Screen::Exit),
+            _ => continue,
+        }
+    }
+}
+
+fn with_blank_screen(start: Print) -> io::Result<()> {
+    enter_blank_screen()?;
+    let mut actual_sceen = start;
+    loop {
+        match actual_sceen() {
+            Err(err) => {
+                exit_blank_screen()?;
+                break Err(err);
+            }
+            Ok(state) => match state {
+                Screen::Next(next_screen) => {
+                    actual_sceen = next_screen;
+                    continue;
+                }
+                Screen::Exit => {
+                    exit_blank_screen()?;
+                    break Ok(());
+                }
+            },
+        }
+    }
+}
+
+fn enter_blank_screen() -> io::Result<()> {
+    queue!(
+        stdout(),
+        terminal::EnterAlternateScreen,
+        terminal::Clear(ClearType::All),
+        terminal::SetTitle("Snake"),
+        cursor::Hide,
+    )?;
+    enable_raw_mode()?;
+    stdout().flush()
+}
+
+fn exit_blank_screen() -> io::Result<()> {
+    disable_raw_mode()?;
+    queue!(stdout(), cursor::Show, terminal::LeaveAlternateScreen)?;
+    stdout().flush()
 }
